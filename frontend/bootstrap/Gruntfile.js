611488ba24cfacaa3c8f7f891169e7aa7222a9d4
Torgeir <twhoffma@gmail.com>
2015-05-01 14:15:43 +0200

    - Local npm-install of grunt-modules - Initial grunt tasks in frontend dir - Simple deploy

--- frontend/bootstrap/Gruntfile.js
+++ frontend/bootstrap/Gruntfile.js
@@ -1,8 +1,26 @@
 module.exports = function(grunt) {
+	
 	//Initializing the configuration object
     grunt.initConfig({
 
         // Task configuration
+		copy: {
+  			main: {
+    			files: [
+      	  	  // includes files within path
+      	  	  		{expand: true, src: ['css/*', 'bower_components/bootstrap/dist/css/*'], dest: 'public/', filter: 'isFile'},
+
+      	  	  		// includes files within path and its sub-directories
+      	  	  		//{expand: true, src: ['path/**'], dest: 'dest/'},
+
+      	  	  		// makes all src relative to cwd
+      	  	  		//{expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+
+      	  	  		// flattens results to a single level
+      	  	  		//{expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+    			],
+  	  		},
+		},
         concat: {
 			options: {
             	separator: ';',
@@ -15,6 +33,17 @@ module.exports = function(grunt) {
         		],
         		dest: './public/js/frontend.js'
       		},
+		},
+        cssmin: {
+			target: {
+				files: [{
+					expand: true,
+      				cwd: 'public/css',
+      				src: ['*.css', '!*.min.css'],
+      				dest: 'public/css',
+      				ext: '.min.css'
+    			}],
+  			},
 		},
         less: {
           //...
@@ -31,12 +60,15 @@ module.exports = function(grunt) {
 	});
 
     // Plugin loading
-  	grunt.loadNpmTasks('../../node_modules/grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+  	grunt.loadNpmTasks('grunt-contrib-concat');
+  	grunt.loadNpmTasks('grunt-contrib-cssmin');
   	//grunt.loadNpmTasks('grunt-contrib-watch');
   	//grunt.loadNpmTasks('grunt-contrib-less');
   	//grunt.loadNpmTasks('grunt-contrib-uglify');
   	//grunt.loadNpmTasks('grunt-phpunit');
-
+	
  	// Task definition
-  	grunt.registerTask('default', ['concat']);
+  	grunt.registerTask('default', ['concat', 'copy']);
+  	grunt.registerTask('prod', ['concat', 'copy', 'cssmin']);
 };
