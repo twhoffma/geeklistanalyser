739f4901e322b85f292b18030ee954f57eb8e482
Torgeir Hoffmann <twhoffma@gmail.com>
2017-04-17 23:54:44 +0200

    - Added allowance for memcached and middleware

--- deploy/initufw.sh
+++ deploy/initufw.sh
@@ -4,8 +4,12 @@ sudo ufw default allow outgoing
 sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
-sudo ufw allow from 127.0.0.1 to 127.0.0.1
+sudo ufw allow from 127.0.0.1 to 127.0.0.1 port 9200
+sudo ufw allow from 127.0.0.1 to 127.0.0.1 port 5984
+sudo ufw allow from 127.0.0.1 to 127.0.0.1 port 11211
+sudo ufw allow from 127.0.0.1 to 127.0.0.1 port 3000
 sudo ufw disable
 sudo ufw enable
 curl 127.0.0.1:5984
 curl 127.0.0.1:9200
+curl 127.0.0.1:11211
