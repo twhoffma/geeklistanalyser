420eb35424b4f6aa3d0ae22fea6f4c22400d321b
Torgeir Hoffmann <twhoffma@gmail.com>
2019-02-10 23:47:44 +0100

    - Trying to setup some backup scripts...

---
+++ deploy/backup.sh
@@ -0,0 +1,6 @@
+couchdb-dump geeklistdb
+
+#TODO: Cleanup using find
+#find /var/backups -type f -name "${media_basename}*" -mtime +$keep -delete
+#dest="/var/backups/${basename}_`date +%Y%m%d-%H%M%S`.sql.xz"
+#password and username for design docs in env var
