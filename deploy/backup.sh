2a008b84429d84a4b736418ab646616ae408ee93
Torgeir Hoffmann <twhoffma@gmail.com>
2019-02-12 00:48:28 +0100

    - Simple backup script

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -1,4 +1,13 @@
-couchdb-dump geeklistdb
+#!/bin/bash
+
+base="$HOME/backup/"
+filebase="-geeklistdb.dump"
+dest="$base`date +%Y%m%d-%H%M%S`$filebase"
+
+couchdb-dump -b 100 "http://127.0.0.1:5984/geeklistdb" > $dest &&
+gzip $dest &&
+s3cmd put "$dest.gz" s3://hoffy-geeklistdb &&
+find $dest -type f -name "*${filebase}.gz" -mtime +30 -delete
 
 #TODO: Cleanup using find
 #find /var/backups -type f -name "${media_basename}*" -mtime +$keep -delete
