1f5bd137f20ad3c670b77d9aa2d8a7d11fa88d28
Torgeir Hoffmann <twhoffma@gmail.com>
2019-04-20 11:18:28 +0200

    - Added rsyslog and logrotation.

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-base="$HOME/backup/"
-filebase="-geeklistdb.dump"
-dest="$base`date +%Y%m%d-%H%M%S`$filebase"
+function glazebackup(){
+	base="$HOME/backup/"
+	filebase="-geeklistdb.dump"
+	dest="$base`date +%Y%m%d-%H%M%S`$filebase"
 
-couchdb-dump -b 100 "http://127.0.0.1:5984/geeklistdb" > $dest 2>"${dest}.log"
+	couchdb-dump -b 100 "http://127.0.0.1:5984/geeklistdb" > $dest 2>"${dest}.log"
 
-echo "zipping couchdb backup"
-gzip $dest 
+	echo "zipping couchdb backup"
+	gzip $dest 
 
-echo "pushing to s3"
-s3cmd put "$dest.gz" s3://hoffy-geeklistdb 
+	echo "pushing to s3"
+	s3cmd --no-progress put "$dest.gz" s3://hoffy-geeklistdb 
 
-echo "Cleaning up old"
-find $base -type f -name "*${filebase}.gz" -mtime +30 -delete
+	echo "Cleaning up old"
+	find $base -type f -name "*${filebase}" -mtime +30 -delete
+	find $base -type f -name "*${filebase}.gz" -mtime +30 -delete
+}
 
-#TODO: Cleanup using find
-#find /var/backups -type f -name "${media_basename}*" -mtime +$keep -delete
-#dest="/var/backups/${basename}_`date +%Y%m%d-%H%M%S`.sql.xz"
-#password and username for design docs in env var
+glazebackup 2>&1|logger -t "geeklistanalyzer-backup" 
