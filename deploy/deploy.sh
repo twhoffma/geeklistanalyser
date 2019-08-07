73ff4a6b7712e3c3caee450f9030f96fcf93eec7
Torgeir Hoffmann <twhoffma@gmail.com>
2019-08-08 00:41:29 +0200

    - Started on docker for elastic. Problems with the init index file as of now..

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,26 +5,29 @@ sudo npm install pm2 -g
 
 sudo apt-get install nginx
 
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+#wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
-echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-1.7.list
+#echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-1.7.list
 
 sudo apt-get update
-sudo apt-get install elasticsearch 
+#sudo apt-get install elasticsearch 
 
-sudo service elasticsearch stop
+#sudo service elasticsearch stop
 
-sudo service start elasticsearch
+#sudo service start elasticsearch
 
 #Install inquisitor for debugging queries
-sudo /usr/share/elasticsearch/bin/plugin -install polyfractal/elasticsearch-inquisitor
+#sudo /usr/share/elasticsearch/bin/plugin -install polyfractal/elasticsearch-inquisitor
 
+#todo: add docker for couchdb.
 
+#docker
+sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:7.3.0  
+sudo docker run -p 9201:9200 -p 9301:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.3.0
 
 #Set services to start on boot - systemd
 sudo /bin/systemctl daemon-reload
 #sudo /bin/systemctl enable elasticsearch.service
-#sudo /bin/systemctl enable logstash.service
 
 #XXX:
 #We have problems running couchdb as a service on Debian 8 since we built it..
--- deploy/elasticsearch/initelastic.sh
+++ deploy/elasticsearch/initelastic.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo "remove boardgames index"
-curl -XDELETE 'http://127.0.0.1:9200/boardgames/'
+#curl -XDELETE 'http://127.0.0.1:9200/boardgames/'
+curl -XDELETE 'http://127.0.0.1:9201/boardgames/'
 
 echo "create boardgames index"
-curl -XPUT 'http://127.0.0.1:9200/boardgames' -d @elastic_index_boardgames.json
+#curl -XPUT 'http://127.0.0.1:9200/boardgames' -d @elastic_index_boardgames.json
+curl -XPUT 'http://127.0.0.1:9201/boardgames' -H 'Content-Type: application/json' -d @elastic_index_boardgames.json
 
