8269312d6310f5eb52469c6bf2761bd9673f550d
Torgeir Hoffmann <twhoffma@gmail.com>
2020-10-28 16:42:27 +0100

    - separated out db, ip and port of couchdb as variables in import_view.sh, fixed bug when inserting new - split geeklistfilter.json into said and geeklisthistograms.json to check if that allows index update to finish with crash.

--- deploy/couchdb/import_view.sh
+++ deploy/couchdb/import_view.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+db="geeklistdb"
+dbip="127.0.0.1"
+dbport="5984"
+
 read ddoc
 
-rev=$(curl "http://127.0.0.1:5984/geeklistdb/_design/$ddoc" | jq -s '.[0]._rev')
+rev=$(curl "http://$dbip:$dbport/$db/_design/$ddoc" | jq -s '.[0]._rev')
 
 if [ "$rev" != "null" ]; then
   echo "$ddoc already exists with revision $rev. Overwrite? [YN]"
   read ow
+else
+  ow="Y"
 fi
 
 if [ $ow = "Y" ]; then
   echo "Importing $ddoc"
-  cat design_docs/$ddoc.json|jq "._rev=$rev" | curl -d @- -H "Content-type: application/json" -X PUT "http://127.0.0.1:5984/geeklistdb/_design/$ddoc"
+  if [ "$rev" != "null"]; then
+    cat design_docs/$ddoc.json | jq "._rev=$rev" | curl -d @- -H "Content-type: application/json" -X PUT "http://$dbip:$dbport/$db/_design/$ddoc"
+  else
+    cat design_docs/$ddoc.json | curl -d @- -H "Content-type: application/json" -X PUT "http://$dbip:$dbport/$db/_design/$ddoc"
+  fi
 fi
