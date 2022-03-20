c5df56b5010c704f5d98fa5b8d036370da1d1668
Torgeir Hoffmann <twhoffma@gmail.com>
2022-03-20 01:10:34 +0100

    - Commented out a zip in backup script that doesn't do anything - disabled old and added new crowd funding lists

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -8,16 +8,16 @@ function glazebackup(){
 	echo "dumping couchdb"	
 	couchdb-dump "http://127.0.0.1:5984/geeklistdb" | xz > "${dest}.xz" 2>/dev/null #"${dest}.log"
 	
-	echo "zipping couchdb backup"
-	gzip $dest 
+	#echo "zipping couchdb backup"
+	#gzip $dest 
 
 	echo "pushing to s3"
-	s3cmd --no-progress put "$dest.gz" s3://hoffy-geeklistdb 
+	s3cmd --no-progress put "${dest}.xz" s3://hoffy-geeklistdb 
 
 	echo "Cleaning up old"
         find $base -type f -name "*${filebase}.log" -mtime +1 -delete
 	find $base -type f -name "*${filebase}" -mtime +3 -delete
-	find $base -type f -name "*${filebase}.gz" -mtime +7 -delete
+	find $base -type f -name "*${filebase}.xz" -mtime +7 -delete
 }
 
 glazebackup 2>&1|logger -t "geeklistanalyzer-backup" 
