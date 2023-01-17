004beecf26db25540920b7afdf4dd2642090c2e9
Torgeir Hoffmann <twhoffma@gmail.com>
2023-01-17 20:19:41 +0100

    - Improved backup script

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -2,14 +2,15 @@
 
 function glazebackup(){
 	base="$HOME/backup/"
-	filebase="-geeklistdb.dump"
+	filebase="-geeklistdb-boardgames.json"
 	dest="$base`date +%Y%m%d-%H%M%S`$filebase"
 	
 	echo "dumping couchdb"	
-	couchdb-dump "http://127.0.0.1:5984/geeklistdb" | xz > "${dest}.xz" 2>/dev/null #"${dest}.log"
-	
-	#echo "zipping couchdb backup"
-	#gzip $dest 
+	#couchdb-dump "http://127.0.0.1:5984/geeklistdb" | xz > "${dest}.xz" 2>/dev/null #"${dest}.log"
+	curl -sS "http://127.0.0.1:5984/geeklistdb/_design/boardgame/_view/boardgame?include_docs=true" -o "${dest}"
+
+	echo "stripping ._rev and compressing"
+	cat "${dest}"|jq '.rows[].doc|del(._rev)'|jq -s '.' | xz > "${dest}.xz"
 
 	echo "pushing to s3"
 	s3cmd --no-progress put "${dest}.xz" s3://hoffy-geeklistdb 
